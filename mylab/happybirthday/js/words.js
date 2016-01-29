54292543010d9169d370c4eaab5d8ff6873a7d7d
Sivory <sivory@qq.com>
2016-01-29 14:22:54 +0800

    加入歌词显示

---
+++ mylab/happybirthday/js/words.js
@@ -0,0 +1,48 @@
+$(function(){
+
+	var audioPlayer = document.getElementById('bg-player');
+	var currentWordText = null;
+	var lastWordText = null;
+	setInterval(function(){
+		var index = -1;
+		for (var i=0;i<wordsList.length;i++) {
+			if (wordsList[i].time <= audioPlayer.currentTime + 0.6 && (i==wordsList.length-1 || wordsList[i+1].time > audioPlayer.currentTime)) {
+				index = i;
+				break;
+			}
+		}
+		if (index>currentIndex) {
+			currentIndex = index;
+			if (currentWordText!=null) {
+				currentWordText.addClass('left');
+			}
+			if (lastWordText!=null) {
+				lastWordText.remove();
+			}
+			lastWordText = currentWordText;
+			currentWordText = $('<div class="word-sample right">').html(wordsList[currentIndex].word);
+			$('body').prepend(currentWordText);
+			setTimeout(function(){
+				currentWordText.removeClass('right');
+			},100);
+		}
+	}, 100);
+
+
+	var wordsList = [];
+	var currentIndex = -1;
+	$.get('./music/bg2.lrc', null, function(response){
+		var wordsTemp = response.split('\n');
+		var reg = /\[(.*)\:(.*)\.(.*)\](.*)/;
+		for (var i=0;i<wordsTemp.length;i++) {
+			var e = reg.exec(wordsTemp[i]);
+			var line = {
+				time: (Number(e[1])*60*1000 + Number(e[2])*1000 + Number(e[3]))/1000,
+				word: e[4]
+			};
+			wordsList[i] = line;
+			console.log(wordsList[i]);
+		}
+		console.log(wordsList);
+	});
+});
