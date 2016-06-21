c0a3cf729b1c63cae6c3377b1c4c1d462cef89a2
Sivory <sivory@qq.com>
2016-06-21 17:54:02 +0800

    体验优化
    优化文字显示，不必显示书中全部文字
    针对不同设备的响应式优化
    增加上次阅读位置记忆功能

---
+++ mylab/A Song of Ice and Fire/logic.js
@@ -0,0 +1,32 @@
+$(function() {
+	var currentIndex = 0;
+	var currentScroll = 0;
+	if (localStorage.getItem('lastIndex') != null) {
+		currentIndex = Number(localStorage.getItem('lastIndex'));
+	}
+	if (localStorage.getItem('lastScroll') != null) {
+		currentScroll = Number(localStorage.getItem('lastScroll'));
+	}
+	var showChapter = function(index) {
+		$('body')[0].scrollTop = 0;
+		$('.p-chapter').addClass('hide');
+		$('.p-title').addClass('hide').removeClass('openable');
+		$('.p-chapter').eq(index).removeClass('hide');
+		$('.p-title').eq(index).removeClass('hide');
+		$('.p-title').eq(index + 1).removeClass('hide').addClass('openable');
+		if (index - 1 >= 0) {
+			$('.p-title').eq(index - 1).removeClass('hide').addClass('openable');
+		}
+	}
+	$('.p-title').click(function(e) {
+		currentIndex = $(e.currentTarget).prevAll('.p-title').length;
+		showChapter(currentIndex);
+	});
+	showChapter(currentIndex);
+	$('body')[0].scrollTop = currentScroll;
+	setInterval(function(){
+		currentScroll = $('body')[0].scrollTop;
+		localStorage.setItem('lastIndex', currentIndex);
+		localStorage.setItem('lastScroll', currentScroll);
+	}, 10000);
+});
